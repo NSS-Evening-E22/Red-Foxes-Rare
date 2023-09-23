17f54f1339279345d017520e82bda599bfe6e671
ahamm <ahammer753@gmail.com>
2023-09-23 12:58:00 -0500

    committing DTO and new endpoint

--- DTO/CreatePostDTO.cs
+++ DTO/CreatePostDTO.cs
@@ -12,8 +12,6 @@ namespace GroupRareAPI.DTO
         public string? ImageUrl { get; set; }
         public string Content { get; set; }
         public bool Approved { get; set; }
-
-        public List<int> TagIds { get; set; }
-
+      
     }
 }
